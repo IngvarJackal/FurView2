9793ee8f4ab1bba06e9805a454d4a7b1ab4e593c
Zey-Uzh <zey-uzh@mail.ru>
2015-09-12 22:08:33 +0400

    Доработано окно добавления алиасов для выделенных тэгов в FullscreenActivity

--- app/src/main/java/ru/furry/furview2/SetAliasesFullscreenActivity.java
+++ app/src/main/java/ru/furry/furview2/SetAliasesFullscreenActivity.java
@@ -1,16 +1,23 @@
 package ru.furry.furview2;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import ru.furry.furview2.database.FurryDatabase;
 import ru.furry.furview2.database.FurryDatabaseUtils;
+import ru.furry.furview2.system.Utils;
 
 
 public class SetAliasesFullscreenActivity extends AppCompatActivity {
@@ -26,19 +33,26 @@ public class SetAliasesFullscreenActivity extends AppCompatActivity {
     FurryDatabaseUtils furryDatabaseUtils;
 
     ArrayList<String> tags;
-    int currentAlyas = 1;
+    int currentAlias = 0;
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        outState.putStringArrayList("tagsForSave",tags);
-        outState.putInt("currentAlyas", currentAlyas);
+        outState.putStringArrayList("tagsForSave", tags);
+        outState.putInt("cAlyas", currentAlias);
         super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         tags = savedInstanceState.getStringArrayList("tagsForSave");
-        currentAlyas = savedInstanceState.getInt("currentAlyas",1);
+        currentAlias = savedInstanceState.getInt("cAlyas", 0);
+        mTagSetAliasesFullscreen.setText(tags.get(currentAlias).toString());
+        if (currentAlias > 0) {
+            int currentAliasplus = currentAlias + 1;
+            setTitle(getResources().getString(R.string.title_activity_aliases) + " (" + currentAliasplus + "/" + tags.size() + ")");
+        } else {
+            setTitle(getResources().getString(R.string.title_activity_aliases) + " (" + 1 + "/" + tags.size() + ")");
+        }
         super.onRestoreInstanceState(savedInstanceState);
     }
 
@@ -48,8 +62,6 @@ public class SetAliasesFullscreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_set_aliases_fullscreen);
 
         tags = getIntent().getStringArrayListExtra("selectedTags");
-
-        setTitle(getResources().getString(R.string.title_activity_aliases) + " ("+ currentAlyas +"/" + tags.size() + ")");
 
         mCancelButton = (Button) findViewById(R.id.cancelButton);
         mSkipButton = (Button) findViewById(R.id.skipButton);
@@ -61,13 +73,77 @@ public class SetAliasesFullscreenActivity extends AppCompatActivity {
         furryDatabase = new FurryDatabase(getApplicationContext());
         furryDatabaseUtils = new FurryDatabaseUtils(furryDatabase);
 
-        mTagSetAliasesFullscreen.setText(tags.get(currentAlyas).toString());
+        mTagSetAliasesFullscreen.setText(tags.get(currentAlias).toString());
+        setTitle(getResources().getString(R.string.title_activity_aliases) + " (" + 1 + "/" + tags.size() + ")");
+
+        mAddLinkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAliasSetAliasesFullscreen.getText().toString().length() > 0) {
+                    Utils.Tuple<String, String> tuple = new Utils.Tuple<String, String>(mTagSetAliasesFullscreen.getText().toString(), mAliasSetAliasesFullscreen.getText().toString());
+                    furryDatabaseUtils.addAlias(tuple);
+                    mAliasSetAliasesFullscreen.clearFocus();
+                    nextAlias();
+                } else {
+                    Toast.makeText(getApplicationContext(), getString(R.string.need_input_alias_fullscreen), Toast.LENGTH_SHORT).show();
+                }
+                //hide keyboard
+                InputMethodManager imm = (InputMethodManager) getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mAliasSetAliasesFullscreen.getWindowToken(), 0);
+                mAliasSetAliasesFullscreen.setText("");
+                mAliasSetAliasesFullscreen.clearFocus();
+            }
+        });
+
+        mAliasSetAliasesFullscreen.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (mAliasSetAliasesFullscreen.getText().toString().length() > 0) {
+                    Utils.Tuple<String, String> tuple = new Utils.Tuple<String, String>(mTagSetAliasesFullscreen.getText().toString(), mAliasSetAliasesFullscreen.getText().toString());
+                    furryDatabaseUtils.addAlias(tuple);
+                    mAliasSetAliasesFullscreen.clearFocus();
+                    nextAlias();
+                } else {
+                    Toast.makeText(getApplicationContext(), getString(R.string.need_input_alias_fullscreen), Toast.LENGTH_SHORT).show();
+                }
+                //hide keyboard
+                InputMethodManager imm = (InputMethodManager) getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mAliasSetAliasesFullscreen.getWindowToken(), 0);
+                mAliasSetAliasesFullscreen.setText("");
+                mAliasSetAliasesFullscreen.clearFocus();
+                return true;
+            }
+        });
+
+        mSkipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAliasSetAliasesFullscreen.setText("");
+                mAliasSetAliasesFullscreen.clearFocus();
+                nextAlias();
+            }
+        });
 
         mCancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //hide keyboard
+                InputMethodManager imm = (InputMethodManager) getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mAliasSetAliasesFullscreen.getWindowToken(), 0);
+                mAliasSetAliasesFullscreen.clearFocus();
                 finish();
             }
         });
+    }
+
+    private void nextAlias() {
+        if (currentAlias == tags.size() - 1) {
+            finish();
+        } else {
+            currentAlias++;
+            int currentAliasplus = currentAlias + 1;
+            setTitle(getResources().getString(R.string.title_activity_aliases) + " (" + currentAliasplus + "/" + tags.size() + ")");
+            mTagSetAliasesFullscreen.setText(tags.get(currentAlias).toString());
+        }
     }
 }
